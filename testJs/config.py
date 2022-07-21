41ef9babfda8e19b724a1ba24954a7338d41fbf1
wuhailong <wuhailong19@huawei.com>
2022-07-21 15:41:22 +0800

    Add js test framework
    Issue:https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I5I7YU?from=project-issue
    
    Signed-off-by: wuhailong <wuhailong19@huawei.com>
    Change-Id: I8e37790dc5c4a73779752104ca2ab439808fe3a6

---
+++ testJs/config.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2022 Huawei Device Co., Ltd.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Description: Use ark to execute test 262 test suite
+"""
+
+import os
+import json
+
+LD_LIBRARY_PATH_LIST = [
+    'out/hispark_taurus/clang_x64/ark/ark',
+    'out/hispark_taurus/clang_x64/ark/ark_js_runtime',
+    'out/hispark_taurus/clang_x64/thirdparty/icu',
+    'prebuilts/clang/ohos/linux-x86_64/llvm/lib'
+]
+
+OUT_DIR = os.path.join("out")
+OUT_TEST_DIR = os.path.join("out", "testJs")
+OUT_RESULT_FILE = os.path.join("out", "testJs", "result.txt")
+TEST_DIR = os.path.join("testJs")
+TS_CASES_DIR = os.path.join(".", "testJs", "test")
+CUR_FILE_DIR = os.path.dirname(__file__)
+CODE_ROOT = os.path.abspath(os.path.join(CUR_FILE_DIR, "../../.."))
+ARK_TS2ABC_PATH = 'arkcompiler/ets_frontend/'
+ARK_JS_VM = './out/hispark_taurus/clang_x64/ark/ark_js_runtime/ark_js_vm'
+ARK_DIR = f"{CODE_ROOT}/out/hispark_taurus/clang_x64/ark/ark"
+WORK_PATH = f'{CODE_ROOT}/{ARK_TS2ABC_PATH}'
+EXPECT_PATH = '_expect.txt'
+DEFAULT_ARK_FRONTEND_TOOL = os.path.join(ARK_DIR, "build", "src", "index.js")
+ARK_FRONTEND_TOOL_ES2ABC = os.path.join(ARK_DIR, "es2abc")
+
+TEST_PATH = os.sep.join([".", "testJs", "test"])
+OUT_PATH = os.sep.join([".", "out", "testJs"])
+JS_EXT = ".js"
+TXT_EXT = ".txt"
+ABC_EXT = ".abc"
---
+++ testJs/utils.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2022 Huawei Device Co., Ltd.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Description: Use ark to execute test 262 test suite
+"""
+
+import os
+import shutil
+from config import *
+import subprocess
+
+
+def command_os(order):
+    subprocess.run(order)
+
+
+def mk_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def remove_dir(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+
+def remove_file(path):
+    if os.path.exists(path):
+        os.remove(path)
+
+
+def read_file(path):
+    util_read_content = []
+    with open(path, "r") as utils_read:
+        util_read_content = utils_read.readlines()
+
+    return util_read_content
+
+
+def excuting_npm_install(args):
+    ark_frontend_tool = os.path.join(DEFAULT_ARK_FRONTEND_TOOL)
+    if args.ark_frontend_tool:
+        ark_frontend_tool = os.path.join(args.ark_frontend_tool)
+
+    ts2abc_build_dir = os.path.join(os.path.dirname(os.path.realpath(ark_frontend_tool)), "..")
+    if os.path.exists(os.path.join(ts2abc_build_dir, "package.json")):
+        npm_install(ts2abc_build_dir)
+    elif os.path.exists(os.path.join(ts2abc_build_dir, "..", "package.json")):
+        npm_install(os.path.join(ts2abc_build_dir, ".."))
+
+
+def npm_install(cwd):
+    try:
+        os.chdir(cwd)
+        command_os(["npm", "install"])
+        os.chdir(WORK_PATH)
+    except BaseException as e:
+        print(e)
+
+
+def process_tool_path(tool_path):
+    if tool_path.startswith('../'):
+        tool_path_list = tool_path.split(os.sep)
+        dir_level = tool_path.count('..')
+        for i in range(dir_level):
+            del tool_path_list[0]
+        tool_path = os.sep.join(tool_path_list)
+        frontend_tool_path = f'{CODE_ROOT}/{tool_path}'
+        return frontend_tool_path
+    return tool_path
+
+
+def export_path():
+    ld_library_path = ':'.join(LD_LIBRARY_PATH_LIST)
+    try:
+        os.chdir(CODE_ROOT)
+        os.environ['LD_LIBRARY_PATH'] = ld_library_path
+        os.chdir(WORK_PATH)
+    except BaseException as e:
+        print(e)
