23e26e4b06eb7e50b08c62da3b8b46dc619ad943
gavin1012_hw <wanggang203@huawei.com>
2022-08-23 23:40:44 +0800

    Support passing proto bin file name && proto bin files' path && @input.txt with paths for merge binary
    Signed-off-by: gavin1012_hw <wanggang203@huawei.com>
    Change-Id: Iaaebf7cb743464ac1ca860b5734a6956dc828f0e

--- merge_abc/src/mergeProgram.cpp
+++ merge_abc/src/mergeProgram.cpp
@@ -20,6 +20,8 @@
 #include "assembler/assembly-function.h"
 #include "libpandafile/literal_data_accessor.h"
 #include <assembly-emitter.h>
+#include <fstream>
+#include "os/file.h"
 
 #if defined(PANDA_TARGET_WINDOWS)
 #include <io.h>
@@ -51,70 +53,6 @@ public:
         MemConfig::Finalize();
     }
 };
-
-int TraverseProtoBinPath(const std::string &protoBinPath, const std::string &protoBinSuffix, MergeProgram *mergeProgram,
-                         panda::ArenaAllocator *allocator)
-{
-    panda::pandasm::Program program;
-
-#if PANDA_TARGET_WINDOWS
-    int handle = 0;
-    struct _finddata_t fileInfo;
-    std::string path;
-    if ((handle = _findfirst(path.assign(protoBinPath).append("\\*").c_str(), &fileInfo)) == -1) {
-        return 1;
-    }
-    do
-    {
-        if (fileInfo.attrib & _A_SUBDIR) {
-            if((!strncmp(fileInfo.name, ".", 1)) || (!strncmp(fileInfo.name, "..", 2))) {
-                continue;
-            }
-            if (TraverseProtoBinPath(path.assign(protoBinPath).append("\\").append(fileInfo.name), protoBinSuffix,
-                                     mergeProgram, allocator) != 0) {
-                _findclose(handle);
-                return 1;
-            }
-        } else {
-            std::string fileName(fileInfo.name);
-            if (fileName.substr(fileName.find_last_of(".") + 1).compare(protoBinSuffix) == 0) {
-                proto::ProtobufSnapshotGenerator::GenerateProgram(
-                    path.assign(protoBinPath).append("\\").append(fileName), program, allocator);
-                mergeProgram->Merge(&program);
-            }
-        }
-    } while (_findnext(handle, &fileInfo) == 0);
-    _findclose(handle);
-#else
-    DIR *protoBin = opendir(protoBinPath.c_str());
-    if (protoBin == nullptr) {
-        return 1;
-    }
-    dirent *dir = nullptr;
-    std::string pathPrefix = protoBinPath + "/";
-    while ((dir = readdir(protoBin)) != nullptr) {
-        if((!strncmp(dir->d_name, ".", 1)) || (!strncmp(dir->d_name, "..", 2))) {
-            continue;
-        }
-        if (dir->d_type == DT_DIR) {
-            std::string subDirName = pathPrefix + dir->d_name;
-            if (TraverseProtoBinPath(subDirName, protoBinSuffix, mergeProgram, allocator) != 0) {
-                closedir(protoBin);
-                return 1;
-            }
-        } else {
-            std::string fileName = pathPrefix + dir->d_name;
-            std::string suffixStr = fileName.substr(fileName.find_last_of(".") + 1);
-            if (suffixStr.compare(protoBinSuffix) == 0) {
-                proto::ProtobufSnapshotGenerator::GenerateProgram(fileName, program, allocator);
-                mergeProgram->Merge(&program);
-            }
-        }
-    }
-    closedir(protoBin);
-#endif
-    return 0;
-}
 
 void MergeProgram::Merge(panda::pandasm::Program *src) {
     CorrectLiteraArrayId(src);
@@ -201,6 +139,136 @@ void MergeProgram::IncreaseInsLiteralArrayIdByBase(panda::pandasm::Ins &insn, si
     }
 }
 
+bool GetProtoFiles(std::string &protoBinPath, std::string &protoBinSuffix, std::vector<std::string> &directoryFiles)
+{
+#if PANDA_TARGET_WINDOWS
+    int handle = 0;
+    struct _finddata_t fileInfo;
+    std::string path;
+    if ((handle = _findfirst(path.assign(protoBinPath).append("\\*").c_str(), &fileInfo)) == -1) {
+        return false;
+    }
+    do
+    {
+        if (fileInfo.attrib & _A_SUBDIR) {
+            if((!strncmp(fileInfo.name, ".", 1)) || (!strncmp(fileInfo.name, "..", 2))) {
+                continue;
+            }
+            if (!GetProtoFiles(path.assign(protoBinPath).append("\\").append(fileInfo.name), protoBinSuffix,
+                               directoryFiles)) {
+                _findclose(handle);
+                return false;
+            }
+        } else {
+            std::string fileName(fileInfo.name);
+            if (fileName.substr(fileName.find_last_of(".") + 1).compare(protoBinSuffix) == 0) {
+                directoryFiles.emplace_back(path.assign(protoBinPath).append("\\").append(fileName));
+            }
+        }
+    } while (_findnext(handle, &fileInfo) == 0);
+    _findclose(handle);
+#elif PANDA_TARGET_UNIX
+    DIR *protoBin = opendir(protoBinPath.c_str());
+    if (protoBin == nullptr) {
+        return false;
+    }
+    dirent *dir = nullptr;
+    std::string pathPrefix = protoBinPath + "/";
+    while ((dir = readdir(protoBin)) != nullptr) {
+        if((!strncmp(dir->d_name, ".", 1)) || (!strncmp(dir->d_name, "..", 2))) {
+            continue;
+        }
+        if (dir->d_type == DT_DIR) {
+            std::string subDirName = pathPrefix + dir->d_name;
+            if (!GetProtoFiles(subDirName, protoBinSuffix, directoryFiles)) {
+                closedir(protoBin);
+                return false;
+            }
+        } else {
+            std::string fileName = pathPrefix + dir->d_name;
+            if (fileName.substr(fileName.find_last_of(".") + 1).compare(protoBinSuffix) == 0) {
+                directoryFiles.emplace_back(fileName);
+            }
+        }
+    }
+    closedir(protoBin);
+#endif
+    return true;
+}
+
+bool AppendProtoFiles(std::string filePath, std::string protoBinSuffix, std::vector<std::string> &protoFiles)
+{
+    auto inputAbs = panda::os::file::File::GetAbsolutePath(filePath);
+    if (!inputAbs) {
+        std::cerr << "Failed to open: " << inputAbs.Value() << std::endl;
+        return false;
+    }
+
+    auto fPath = inputAbs.Value();
+    if (panda::os::file::File::IsRegularFile(fPath)) {
+        if (filePath.substr(filePath.find_last_of(".") + 1).compare(protoBinSuffix) == 0) {
+            protoFiles.emplace_back(fPath);
+        }
+    } else if (panda::os::file::File::IsDirectory(fPath)) {
+        std::vector<std::string> directoryFiles;
+        if (!GetProtoFiles(fPath, protoBinSuffix, directoryFiles)) {
+            return false;
+        }
+        protoFiles.insert(protoFiles.end(), directoryFiles.begin(), directoryFiles.end());
+    } else {
+        std::cerr << "Input must be either a regular file or directory" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool CollectProtoFiles(std::string input, std::string protoBinSuffix, std::vector<std::string> &protoFiles)
+{
+    constexpr const char DOGGY = '@';
+    std::vector<std::string> inputs;
+    bool isList = false;
+
+    if (input[0] == DOGGY) {
+        input.erase(input.begin() + 0);
+        isList = true;
+    }
+
+    auto inputAbs = panda::os::file::File::GetAbsolutePath(input);
+    if (!inputAbs) {
+        std::cerr << "Failed to open: " << inputAbs.Value() << std::endl;
+        return false;
+    }
+    if (isList) {
+        std::ifstream in(inputAbs.Value());
+        std::string line;
+        constexpr const char CARRIAGE = '\r';
+        while (getline(in, line))
+        {
+            // erase front spaces
+            line.erase(line.begin(),
+                       std::find_if(line.begin(), line.end(), [](unsigned char ch) { return std::isspace(ch) == 0; }));
+            // erase carrige return symbol (Windows workaround)
+            line.erase(std::find_if(line.rbegin(), line.rend(), [](unsigned char ch) { return ch != CARRIAGE; }).base(),
+                       line.end());
+            if (!line.empty()) {
+                inputs.push_back(line);
+            }
+        }
+        in.close();
+    } else {
+        inputs.push_back(inputAbs.Value());
+    }
+
+    for (auto &filePath : inputs) {
+        if (!AppendProtoFiles(filePath, protoBinSuffix, protoFiles)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int Run(int argc, const char **argv)
 {
     auto options = std::make_unique<Options>();
@@ -209,15 +277,29 @@ int Run(int argc, const char **argv)
         return 1;
     }
 
-    std::string protoBinPath = options->protoBinPath();
+    std::string protoPathInput = options->protoPathInput();
     std::string protoBinSuffix = options->protoBinSuffix();
+    std::string outputFilePath = options->outputFilePath();
+
+    if (outputFilePath.empty()) {
+        outputFilePath = panda::os::file::File::GetExecutablePath().Value().append(
+            panda::os::file::File::GetPathDelim());
+    }
 
     panda::ArenaAllocator allocator(panda::SpaceType::SPACE_TYPE_COMPILER, nullptr, true);
 
+    std::vector<std::string> protoFiles;
+    if (!CollectProtoFiles(protoPathInput, protoBinSuffix, protoFiles)) {
+        return 1;
+    }
+
     panda::pandasm::Program program;
     MergeProgram mergeProgram(&program);
-    if (panda::proto::TraverseProtoBinPath(protoBinPath, protoBinSuffix, &mergeProgram, &allocator)) {
-        return 1;
+
+    for (auto &protoFile : protoFiles) {
+        panda::pandasm::Program program;
+        proto::ProtobufSnapshotGenerator::GenerateProgram(protoFile, program, &allocator);
+        mergeProgram.Merge(&program);
     }
 
     std::map<std::string, size_t> stat;
@@ -225,13 +307,8 @@ int Run(int argc, const char **argv)
     panda::pandasm::AsmEmitter::PandaFileToPandaAsmMaps maps {};
     panda::pandasm::AsmEmitter::PandaFileToPandaAsmMaps *mapsp = nullptr;
 
-    std::string outputPandaFile = options->outputPandaFile();
-#ifdef PANDA_TARGET_WINDOWS
-    outputPandaFile = protoBinPath + "\\" + outputPandaFile;
-#else
-    outputPandaFile = protoBinPath + "/" + outputPandaFile;
-#endif
-    if (!panda::pandasm::AsmEmitter::Emit(outputPandaFile, *(mergeProgram.GetResult()), statp, mapsp, true)) {
+    std::string outputFileName = outputFilePath.append(options->outputFileName());
+    if (!panda::pandasm::AsmEmitter::Emit(outputFileName, *(mergeProgram.GetResult()), statp, mapsp, true)) {
         return 1;
     }
 
