2e17e5f22e4c293c454f24ce71cca22ff517839b
xucheng46 <xucheng46@huawei.com>
2022-10-12 17:53:39 +0800

    Fix parse of type assersion expression in await
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I5VD3W
    Test: parse tests, compiler tests, test262
    
    Signed-off-by: xucheng46 <xucheng46@huawei.com>
    Change-Id: I4a7c145dbf2f275f02c51faa421db99b4b4b61e7

---
+++ es2panda/test/parser/ts/test-await.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+async function f() {
+    await 0;
+    void await 0;
+    await await 0;
+    <number> await 0;
+    <number> void await 0;
+    typeof await 0;
+    typeof void await 0;
+    <string> typeof <number> void await 0;
+    await <string> typeof <number> void await 0;
+}
