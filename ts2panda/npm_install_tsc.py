39ac9eeeaae4509948b087653db0b930ed1965f2
zhangchen168 <zhangchen168@huawei.com>
2023-05-10 14:33:44 +0800

    mkdir deps folder
    Signed-off-by: zhangchen168 <zhangchen168@huawei.com>
    Change-Id: I7283f06b9763113c9a29968222bf3f3e37fcc341

--- ts2panda/npm_install_tsc.py
+++ ts2panda/npm_install_tsc.py
@@ -2,22 +2,39 @@
 
 import sys
 import subprocess
+import os
 
 def run_cmd(cmd, execution_path = None):
-    res = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                            stdin=subprocess.PIPE,
                            stderr=subprocess.PIPE,
                            cwd=execution_path,
                            shell=True)
-    sout, serr = res.communicate()
-    res.wait()
-    return res.pid, res.returncode, sout, serr
+    stdout, stderr = proc.communicate()
+    if proc.returncode != 0:
+        print(stdout.decode(), stderr.decode())
+        raise Exception(stderr.decode())
 
 def run(args):
-    tsc_path =args[0]
-    cmd = " ".join(["npm", "install", tsc_path, "--legacy-peer-deps"])
-    run_cmd(cmd, args[1])
-    run_cmd(cmd, args[2])
+    tsc_path = args[0]
+    cur_path = args[2]
+    deps_path = os.path.join(cur_path, "deps")
+    if not os.path.exists(deps_path):
+        os.makedirs(deps_path, exist_ok= True)
+
+    tsc_package_path = os.path.join(deps_path, "typescript-4.2.3-r2.tgz")
+    if not os.path.exists(tsc_package_path):
+        cmd = " ".join(["cp", "-f", tsc_path, deps_path])
+        run_cmd(cmd)
+    
+    if not os.path.exists(os.path.join(args[1], "node_modules", "typescript")):
+        cmd = " ".join(["npm", "install", tsc_path, "--legacy-peer-deps"])
+        run_cmd(cmd, args[1])
+
+    if not os.path.exists(os.path.join(cur_path, "node_modules", "typescript")):
+        tsc_cur_path = os.path.join(deps_path, "typescript-4.2.3-r2.tgz")
+        cmd = " ".join(["npm", "install", tsc_cur_path, "--legacy-peer-deps"])
+        run_cmd(cmd, cur_path)
 
 if __name__ == "__main__":
     run(sys.argv[1:])
