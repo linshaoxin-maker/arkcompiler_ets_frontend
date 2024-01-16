5f38ddcfe1dda7922bd3c23c9b3a19d7987838a7
Soma Simon <soma.simon@huawei.com>
2024-01-16 11:32:09 +0100

    [ArkTS frontend] Fix cannot reference 'this' in this context
    Save context at proxy methods to fix the problem with this expression. Create a better check if a proxy method has return type.
    
    Fixes internal issue #14623
    
    Change-Id: I03f41a1e90a6bde387ab504ce886694a69f8a40f
    Signed-off-by: Soma Simon <soma.simon@huawei.com>

--- ets2panda/parser/ETSparser.cpp
+++ ets2panda/parser/ETSparser.cpp
@@ -2202,7 +2202,7 @@ ir::MethodDefinition *ETSParser::CreateProxyMethodDefinition(ir::MethodDefinitio
     }
 
     proxyMethod += ' ';
-    if (returnType != "void") {
+    if ((function->AsScriptFunction()->Flags() & ir::ScriptFunctionFlags::HAS_RETURN) != 0) {
         proxyMethod += "return ";
     }
 
