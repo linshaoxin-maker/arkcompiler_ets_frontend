e6b5abfa8d7d44423493a2646a4790839881a535
Molokanov Yaroslav <molokanov.yaroslav@huawei-partners.com>
2024-01-29 18:50:08 +0300

    ArithmeticOperationValidForAll check in AST Verifier
    Signed-off-by: Molokanov Yaroslav <molokanov.yaroslav@huawei-partners.com>

--- ets2panda/compiler/core/ASTVerifier.cpp
+++ ets2panda/compiler/core/ASTVerifier.cpp
@@ -93,7 +93,7 @@ private:
     util::StringView checkName_;
 };
 
-static bool IsNumericType(const ir::AstNode *ast)
+static bool IsBooleanType(const ir::AstNode *ast)
 {
     if (ast == nullptr) {
         return false;
@@ -109,8 +109,49 @@ static bool IsNumericType(const ir::AstNode *ast)
         return false;
     }
 
+    if (typedAst->TsType()->HasTypeFlag(checker::TypeFlag::ETS_OBJECT) &&
+        ast->HasBoxingUnboxingFlags(ir::BoxingUnboxingFlags::UNBOXING_FLAG)) {
+        return typedAst->TsType()->AsETSObjectType()->HasObjectFlag(checker::ETSObjectFlags::BUILTIN_BOOLEAN);
+    }
+
+    return typedAst->TsType()->HasTypeFlag(checker::TypeFlag::ETS_BOOLEAN) ||
+           typedAst->TsType()->HasTypeFlag(checker::TypeFlag::BOOLEAN_LIKE);
+}
+
+static bool IsValidTypeForBinaryOp(const ir::AstNode *ast, bool isBitwise)
+{
+    if (ast == nullptr) {
+        return false;
+    }
+
+    if (!ast->IsTyped()) {
+        return false;
+    }
+
+    auto typedAst = static_cast<const ir::TypedAstNode *>(ast);
+
+    if (typedAst->TsType() == nullptr) {
+        return false;
+    }
+
+    if (IsBooleanType(ast)) {
+        return isBitwise;
+    }
+
+    if (typedAst->TsType()->HasTypeFlag(checker::TypeFlag::ETS_OBJECT) &&
+        typedAst->TsType()->AsETSObjectType()->HasObjectFlag(checker::ETSObjectFlags::BUILTIN_BIGINT)) {
+        return true;
+    }
+
+    if (typedAst->TsType()->HasTypeFlag(checker::TypeFlag::ETS_OBJECT) &&
+        ast->HasBoxingUnboxingFlags(ir::BoxingUnboxingFlags::UNBOXING_FLAG)) {
+        return typedAst->TsType()->AsETSObjectType()->HasObjectFlag(checker::ETSObjectFlags::BUILTIN_TYPE) &&
+               !typedAst->TsType()->AsETSObjectType()->HasObjectFlag(checker::ETSObjectFlags::BUILTIN_BOOLEAN);
+    }
+
     return typedAst->TsType()->HasTypeFlag(checker::TypeFlag::ETS_NUMERIC) ||
            typedAst->TsType()->HasTypeFlag(checker::TypeFlag::NUMBER_LITERAL) ||
+           typedAst->TsType()->HasTypeFlag(checker::TypeFlag::BIGINT) ||
            typedAst->TsType()->HasTypeFlag(checker::TypeFlag::BIGINT_LITERAL);
 }
 
@@ -128,6 +169,11 @@ static bool IsStringType(const ir::AstNode *ast)
 
     if (typedAst->TsType() == nullptr) {
         return false;
+    }
+
+    if (typedAst->TsType()->HasTypeFlag(checker::TypeFlag::ETS_OBJECT)) {
+        return typedAst->TsType()->AsETSObjectType()->HasObjectFlag(checker::ETSObjectFlags::STRING) ||
+               typedAst->TsType()->AsETSObjectType()->HasObjectFlag(checker::ETSObjectFlags::BUILTIN_STRING);
     }
 
     return typedAst->TsType()->HasTypeFlag(checker::TypeFlag::STRING_LIKE);
@@ -853,22 +899,23 @@ public:
 
     [[nodiscard]] CheckResult operator()([[maybe_unused]] CheckContext &ctx, const ir::AstNode *ast)
     {
-        if (ast->IsBinaryExpression() && ast->AsBinaryExpression()->IsArithmetic()) {
-            if (ast->AsBinaryExpression()->OperatorType() == lexer::TokenType::PUNCTUATOR_PLUS &&
-                IsStringType(ast->AsBinaryExpression()->Left()) && IsStringType(ast->AsBinaryExpression()->Right())) {
-                return {CheckDecision::CORRECT, CheckAction::CONTINUE};
-            }
-            auto result = std::make_tuple(CheckDecision::CORRECT, CheckAction::CONTINUE);
-            ast->Iterate([&result, &ctx](ir::AstNode *child) {
-                if (!IsNumericType(child)) {
-                    ctx.AddCheckMessage("Not a numeric type", *child, child->Start());
-                    result = {CheckDecision::INCORRECT, CheckAction::CONTINUE};
-                }
-            });
-            return result;
+        if (!ast->IsBinaryExpression() || !ast->AsBinaryExpression()->IsArithmetic()) {
+            return {CheckDecision::CORRECT, CheckAction::CONTINUE};
         }
-
-        return {CheckDecision::CORRECT, CheckAction::CONTINUE};
+        if ((ast->AsBinaryExpression()->OperatorType() == lexer::TokenType::PUNCTUATOR_PLUS ||
+             ast->AsBinaryExpression()->OperatorType() == lexer::TokenType::PUNCTUATOR_PLUS_EQUAL) &&
+            (IsStringType(ast->AsBinaryExpression()->Left()) || IsStringType(ast->AsBinaryExpression()->Right()))) {
+            return {CheckDecision::CORRECT, CheckAction::CONTINUE};
+        }
+        auto result = std::make_tuple(CheckDecision::CORRECT, CheckAction::CONTINUE);
+        bool isBitwise = ast->AsBinaryExpression()->IsBitwise();
+        ast->Iterate([&result, &ctx, &isBitwise](ir::AstNode *child) {
+            if (!IsValidTypeForBinaryOp(child, isBitwise)) {
+                ctx.AddInvariantError("ArithmeticOperationValid", "Not a valid type", *child);
+                result = {CheckDecision::INCORRECT, CheckAction::CONTINUE};
+            }
+        });
+        return result;
     }
 
 private:
