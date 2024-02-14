d452d76c49d7c9af53c4b70defc2008c07462dbf
Svetlana Rokashevich <rokashevich.svetlana@huawei-partners.com>
2024-02-14 23:19:27 +0300

    try to set +e
    Signed-off-by: Svetlana Rokashevich <rokashevich.svetlana@huawei-partners.com>

--- ets2panda/scripts/es2panda_coverage.sh
+++ ets2panda/scripts/es2panda_coverage.sh
@@ -28,13 +28,15 @@ case "$ARGUMENT" in
 esac
 done
 
-# source $PANDA_ROOT/scripts/python/venv-utils.sh
-# activate_venv
+source $PANDA_ROOT/scripts/python/venv-utils.sh
+activate_venv
+set +e
 
 python3 $PANDA_ROOT/tools/es2panda/scripts/test_runner.py \
     --builddir $PANDA_BINARY_ROOT --arkdir $PANDA_ROOT --all
 
-# deactivate_venv
+set -e
+deactivate_venv
 
 gcov $PANDA_BINARY_ROOT/tools/es2panda/CMakeFiles/es2panda-lib.dir/*/*
 
