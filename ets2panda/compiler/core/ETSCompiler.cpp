0a8338c808ee4406d982810e7eb43017ea802665
Torok Gergo <gergo.torok@huawei.com>
2024-05-21 10:07:09 +0200

    [ETS] Updating type alias
    Issue:
    https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I9U38V
    
    Reason:
    Type alias doesn't work correctly.
    It should allowed to be declared in function scopes and with the same name as variable that is not an other type alias.
    
    Description:
    Added a local scope for type aliases to the function scope to separate them from other variables.
    Changes in function scope variable binding
    
    Signed-off-by: Gergo Torok <gergo.torok@huawei.com>

--- ets2panda/compiler/core/ETSCompiler.cpp
+++ ets2panda/compiler/core/ETSCompiler.cpp
@@ -2266,10 +2266,7 @@ void ETSCompiler::Compile([[maybe_unused]] const ir::TSTupleType *node) const
     UNREACHABLE();
 }
 
-void ETSCompiler::Compile([[maybe_unused]] const ir::TSTypeAliasDeclaration *st) const
-{
-    UNREACHABLE();
-}
+void ETSCompiler::Compile([[maybe_unused]] const ir::TSTypeAliasDeclaration *st) const {}
 
 void ETSCompiler::Compile([[maybe_unused]] const ir::TSTypeAssertion *expr) const
 {
