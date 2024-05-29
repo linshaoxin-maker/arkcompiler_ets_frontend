e95e0cc9bda0feea9b462376c4fa87cb8907cb0f
zhuangkudecha <fuhao71@huawei.com>
2024-05-29 15:44:11 +0800

    Implement Record Indexing Expression
    Inherit Record from Map, add tests for Record Indexing and Record key type check, unignore some Record related tests
    
    Issue:https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I9OIMZ
    Test:local test suite
    Signed-off-by: zhuangkudecha <fuhao71@huawei.com>

--- ets2panda/checker/ets/helpers.cpp
+++ ets2panda/checker/ets/helpers.cpp
@@ -608,6 +608,25 @@ checker::Type *ETSChecker::FixOptionalVariableType(varbinder::Variable *const bi
     return bindingVar->TsType();
 }
 
+checker::Type *PreferredObjectTypeFromAnnotation(checker::Type *annotationType)
+{
+    if (!annotationType->IsETSUnionType()) {
+        return annotationType;
+    }
+
+    checker::Type *resolvedType = nullptr;
+    for (auto constituentType : annotationType->AsETSUnionType()->ConstituentTypes()) {
+        if (constituentType->IsETSObjectType()) {
+            if (resolvedType != nullptr) {
+                return nullptr;
+            }
+            resolvedType = constituentType;
+        }
+    }
+
+    return resolvedType;
+}
+
 checker::Type *ETSChecker::CheckVariableDeclaration(ir::Identifier *ident, ir::TypeNode *typeAnnotation,
                                                     ir::Expression *init, ir::ModifierFlags const flags)
 {
@@ -658,7 +677,7 @@ checker::Type *ETSChecker::CheckVariableDeclaration(ir::Identifier *ident, ir::T
     }
 
     if (init->IsObjectExpression()) {
-        init->AsObjectExpression()->SetPreferredType(annotationType);
+        init->AsObjectExpression()->SetPreferredType(PreferredObjectTypeFromAnnotation(annotationType));
     }
 
     InferAliasLambdaType(typeAnnotation, init);
