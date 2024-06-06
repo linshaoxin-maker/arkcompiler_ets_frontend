13ca8bcdd199b0a724239426327850c81f9be60e
yuxin <wangyuxin53@huawei.com>
2024-06-06 20:26:00 +0800

    Description：support Recursive generics for class
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I9SQYG
    
    Tests:
    
    ninja es2panda_testsbash
    ets_testtunner.sh --cts -r -rt
    
    Signed-off-by: yuxin <wangyuxin53@huawei.com>

--- ets2panda/checker/ets/function.cpp
+++ ets2panda/checker/ets/function.cpp
@@ -1015,7 +1015,8 @@ SignatureInfo *ETSChecker::ComposeSignatureInfo(ir::ScriptFunction *func)
     }
 
     if (func->TypeParams() != nullptr) {
-        signatureInfo->typeParams = CreateTypeForTypeParameters(func->TypeParams());
+        signatureInfo->typeParams = CreateUnconstrainedTypeParameters(func->TypeParams());
+        AssignTypeParameterConstraints(func->TypeParams());
     }
 
     for (auto *const it : func->Params()) {
@@ -3453,6 +3454,16 @@ void ETSChecker::CacheFunctionalInterface(ir::ETSFunctionType *type, ETSObjectTy
     auto hash = GetHashFromFunctionType(type);
     ASSERT(functionalInterfaceCache_.find(hash) == functionalInterfaceCache_.cend());
     functionalInterfaceCache_.emplace(hash, ifaceType);
+}
+
+ArenaVector<ConstraintCheckRecord> &ETSChecker::PendingConstraintCheckRecords()
+{
+    return pendingConstraintCheckRecords_;
+}
+
+size_t &ETSChecker::ConstraintCheckScopesCount()
+{
+    return constraintCheckScopesCount_;
 }
 
 }  // namespace ark::es2panda::checker
