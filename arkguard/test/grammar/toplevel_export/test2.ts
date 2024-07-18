a60cea6a937e9aee496a8a30a254ac06517411fe
dengxinyu <dengxinyu4@huawei.com>
2024-07-18 22:26:04 +0800

    Fix the bug that toplevel elements cannot be obfuscated properly
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IAE07T
    
    Test: npm run test & ut & application test & automation test
    
    Signed-off-by: dengxinyu <dengxinyu4@huawei.com>
    Change-Id: I7b18767004f772eed27c16b04d4813e7b951e827

---
+++ arkguard/test/grammar/toplevel_export/test2.ts
@@ -0,0 +1,32 @@
+
+export let importVar1 = 1;
+export let importVar2 = 1;
+export namespace ns{
+    export let importVar3 = 1;
+}
+
+function foo2(){}
+export type A ={
+
+}
+
+export type C ={
+
+}
+
+export type D ={
+
+}
+
+let var1 = 1;
+let var2 = 2;
+let var3 = 3;
+
+export {var1 as abc, var2, var3};
+export {foo2 as default};
+export {var10 as ccc} from './test1'
+export {default as ddd} from './test1'
+export {var11 as eee} from './test1'
+
+
+
