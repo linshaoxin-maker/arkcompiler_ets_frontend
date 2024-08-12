0718fe2c0ff6c31b796fc74939f053ef0dff0e5a
zhangchen <zhangchen168@huawei.com>
2024-08-12 21:20:11 +0800

    delete the useless api
    Signed-off-by: zhangchen <zhangchen168@huawei.com>
    Change-Id: I9a91d65ee56ecf759455c33663cf585c72de8889

---
+++ arkguard/src/common/ApiReaderForTest.ts
@@ -0,0 +1,53 @@
+import { ArkObfuscator } from '../ArkObfuscator';
+import { IOptions } from '../configs/IOptions';
+import { ReseverdSetForArkguard, readProjectPropertiesByCollectedPaths } from "./ApiReader";
+import fs from 'fs';
+import path from 'path'
+
+/**
+ * 递归获取文件夹下的所有子文件路径
+ * @param dir - 需要遍历的文件夹路径
+ * @returns 返回所有子文件路径的数组
+ */
+function getAllFiles(inputPaths: string[]): Set<string> {
+  let results: Set<string> = new Set();
+  for (let inputPath of inputPaths) {
+    const stat = fs.statSync(inputPath);
+    if (stat.isDirectory()) {
+      recursiveRead(inputPath);
+    } else if (stat.isFile()) {
+      results.add(inputPath);
+    }
+  }
+  function recursiveRead(dir: string): void {
+    const list = fs.readdirSync(dir);
+    list.forEach((file) => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat && stat.isDirectory()) {
+        recursiveRead(filePath);
+      } else {
+        results.add(filePath);
+      }
+    });
+  }
+
+  return results;
+}
+
+/**
+ * read project reserved properties for UT
+ * @param projectPaths can be dir or file
+ * @param customProfiles
+ */
+export function readProjectProperties(projectPaths: string[], customProfiles: IOptions, arkObfuscator: ArkObfuscator): void {
+  const allPaths: Set<string> = getAllFiles(projectPaths);
+  let projectAndLibs: ReseverdSetForArkguard = readProjectPropertiesByCollectedPaths(allPaths, customProfiles, false);
+  if (customProfiles.mNameObfuscation.mRenameProperties) {
+    arkObfuscator.addReservedSetForPropertyObf(projectAndLibs);
+  }
+  if (customProfiles.mExportObfuscation) {
+    arkObfuscator.addReservedSetForDefaultObf(projectAndLibs);
+  }
+}
