7ea154475fec9929a6e82984ff96ecfe9b46f7b0
Denis Anisimov <denis.anisimov@omega-team.ru>
2024-09-11 05:57:57 +0000

    Title: [ASTVerifier] Add check for readonly flags
    Description: Added new AST Verifier check that verifies that all readonly fields in classes have readonly flags instead of const.
    
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IAIB3E
    Testing: ets_frontend/ets2panda/test/unit/public/ast_verifier_check_readonly_fields_test.cpp
    
    Signed-off-by: Denis Anisimov <johndoe335827@gmail.com>

--- ets2panda/checker/ets/dynamic.cpp
+++ ets2panda/checker/ets/dynamic.cpp
@@ -387,9 +387,8 @@ ir::ClassDeclaration *ETSChecker::BuildClass(util::StringView name, const ClassB
 ir::ClassProperty *ETSChecker::CreateStaticReadonlyField(const char *name)
 {
     auto *fieldIdent = AllocNode<ir::Identifier>(name, Allocator());
-    // NOTE: remove const when readonly is properly supported
     auto flags =
-        ir::ModifierFlags::STATIC | ir::ModifierFlags::PRIVATE | ir::ModifierFlags::READONLY | ir::ModifierFlags::CONST;
+        ir::ModifierFlags::STATIC | ir::ModifierFlags::PRIVATE | ir::ModifierFlags::READONLY;
     auto *field = AllocNode<ir::ClassProperty>(
         fieldIdent, nullptr, AllocNode<ir::ETSPrimitiveType>(ir::PrimitiveType::INT), flags, Allocator(), false);
 
@@ -608,9 +607,7 @@ void ETSChecker::BuildDynamicImportClass()
                 imports.push_back(import);
 
                 auto *fieldIdent = AllocNode<ir::Identifier>(import->AssemblerName(), Allocator());
-                // NOTE: remove const when readonly is properly supported
-                auto flags = ir::ModifierFlags::STATIC | ir::ModifierFlags::PUBLIC | ir::ModifierFlags::READONLY |
-                             ir::ModifierFlags::CONST;
+                auto flags = ir::ModifierFlags::STATIC | ir::ModifierFlags::PUBLIC | ir::ModifierFlags::READONLY;
                 auto *field = AllocNode<ir::ClassProperty>(
                     fieldIdent, nullptr, AllocNode<ir::OpaqueTypeNode>(GlobalBuiltinDynamicType(import->Language())),
                     flags, Allocator(), false);
