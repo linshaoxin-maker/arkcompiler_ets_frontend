78a514df0d39f4cceac77800b38abfd03c9476c2
groshevmaksim <groshev.maksim@h-partners.com>
2024-09-30 12:00:48 +0300

    Title: Fixing of union assignement
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IAUT7H
    
    Signed-off-by: groshevmaksim <groshev.maksim@h-partners.com>

--- ets2panda/checker/types/ets/etsUnionType.cpp
+++ ets2panda/checker/types/ets/etsUnionType.cpp
@@ -415,7 +415,7 @@ checker::Type *ETSUnionType::GetAssignableType(checker::ETSChecker *checker, che
         }
     }
 
-    return nullptr;
+    return checker->GlobalTypeError();
 }
 
 checker::Type *ETSUnionType::GetAssignableBuiltinType(
