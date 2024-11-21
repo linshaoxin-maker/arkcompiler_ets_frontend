8136fff2be0cced1868372587848e7bfc632755a
xingshunxiang <xingshunxiang@huawei.com>
2024-11-21 11:29:17 +0800

    tmp
    Signed-off-by: xingshunxiang <xingshunxiang@huawei.com>

--- ets2panda/varbinder/scope.cpp
+++ ets2panda/varbinder/scope.cpp
@@ -955,7 +955,7 @@ Variable *ClassScope::AddBinding(ArenaAllocator *allocator, [[maybe_unused]] Var
 
     const auto *foundVar = FindLocal(newDecl->Name(), options);
     if (foundVar != nullptr) {
-        if (!newDecl->IsLetOrConstDecl()) {
+        if (!newDecl->IsLetOrConstDecl() && !newDecl->IsReadonlyDecl()) {
             return nullptr;
         }
 
