b3a390a09ca321b1fd1a6a51962fea528fbb2c87
peiyifan <peiyifan3@huawei.com>
2024-11-21 15:54:29 +0800

    Add panda_guard Signed-off-by: dapeizi <peiyifan3@huawei.com>

--- panda_guard/tests/unittest/guard_context_test.cpp
+++ panda_guard/tests/unittest/guard_context_test.cpp
@@ -41,20 +41,3 @@ HWTEST(GuardContextUnitTest, guard_context_test_001, TestSize.Level4)
     context->Init(argc, const_cast<const char **>(argv));
     EXPECT_EQ(context->IsDebugMode(), true);
 }
-
-/**
- * @tc.name: guard_context_test_002
- * @tc.desc: test context init with invalid config
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST(GuardContextUnitTest, guard_context_test_002, TestSize.Level4)
-{
-    int argc = 2;
-    char *argv[2];
-    argv[0] = const_cast<char *>("xxx");
-    argv[1] = const_cast<char *>("xxx");
-
-    auto context = guard::GuardContext::GetInstance();
-    EXPECT_DEATH(context->Init(argc, const_cast<const char **>(argv)), "");
-}
