9b891cb6b557076429f1801fb9ba8308f7eba6a5
zengyuan <zengyuan22@huawei.com>
2024-11-29 15:38:47 +0800

    testcases
    Signed-off-by: zengyuan <zengyuan22@huawei.com>
    Change-Id: Ib058d9fbeedf7c0ad5f09da8fd20135a997a62af

---
+++ arkguard/test/grammar/print_unobfuscation/unobfuscation_source2/unobfuscation_source2.ts
@@ -0,0 +1,46 @@
+
+
+// -enable-property-obfuscation
+// lang
+
+// export
+export let export1 = 1;
+export class MyExport {
+    exportedProp = 1;
+}
+export let usb = 3; // lang
+
+// string property
+class MyString {
+    'string1' = 1;
+    'string2' = 2;
+    'unit' = 3; // lang
+}
+
+// toplevel
+function toplevel1(): number {
+    return 1;
+}
+function toplevel2(): number {
+    return 2;
+}
+
+// property
+class MyClass {
+    property1 = 1;
+    exportedProp = 1; //export
+    a = 2; // lang
+    peoperty2(){}
+    string1 = '1';  // string
+}
+
+// local variable and parameter
+function add(param1: number, param2: number): number { // lang
+    let variable1 = param1;
+    let variable2 = param2;
+    return variable1 + variable2;
+}
+
+function myfunc(input: number): number {
+    return input + 1;
+}
